2d0a38d4980428712587dad31257bbc588eee687
FBRTMaka <mjmcee@gmail.com>
2016-06-11 14:31:16 -0400

    update

--- client/scripts/load-css.js
+++ client/scripts/load-css.js
@@ -1,17 +1,17 @@
 // LOAD CUSTOM CSS
-// ----------------------------------- 
+// -----------------------------------
 
 (function(window, document, $, undefined){
 
   $(function(){
 
     $(document).on('click', '[data-load-css]', function (e) {
-        
+
       var element = $(this);
 
       if(element.is('a'))
         e.preventDefault();
-      
+
       var uri = element.data('loadCss'),
           link;
 
